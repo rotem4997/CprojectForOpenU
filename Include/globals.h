16f25b4ba4ba1ad41c1e8e41dea53ed10d9ada08
rotem4997 <68532652+rotem4997@users.noreply.github.com>
2022-08-10 20:58:29 +0300

    Update globals.h

--- Include/globals.h
+++ Include/globals.h
@@ -20,6 +20,11 @@
 #define IC_INIT_VALUE 100 /* Initial IC Value */
 #define MAX_LINE_LENGTH 80 /* Maximum length of a source of a single line */
 
+#define END_OF_MACRO "endmacro"
+#define START_OF_MACRO "macro"
+#define ERROR_IN_LINE "error in %s: %s\n"
+#define MAX_MACRO_LINE_LENGTH 6
+#define MAX_SYMBOL_LENGTH 31
 
 /** Boolean definition true or false */
 typedef enum booleans 
@@ -208,6 +213,22 @@ typedef struct machine_instruction_line {
     char *source_operand;
     char *destination_operand;
 } machine_instruction_line;
+
+typedef struct macro{
+    char *macro_id;
+    char *macro_content;
+    struct macro *next;
+}macro;
+
+typedef struct line_info{
+    char *file_name;
+    int *line_num;
+    char *content;
+}line_info;
+
+
+
+
 
 
 #endif
