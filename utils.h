6b6a96c6ebbf74cdf3f29792e568f48b05946fbe
rotem4997 <68532652+rotem4997@users.noreply.github.com>
2022-08-27 12:16:39 +0300

    Update utils.h
    Documentation completed

--- utils.h
+++ utils.h
@@ -1,6 +1,6 @@
 #include "structs.h"
 
-/* Helper functions that are used for parsing tokens and navigating through them */
+/**Functions that are used for parsing tokens and moving within them */
 char *next_token_string(char *dest, char *line);
 char *next_list_token(char *dest, char *line);
 char *next_token(char *seq);
@@ -9,7 +9,7 @@ void copy_token(char *dest, char *line);
 int end_of_line(char *line);
 int ignore(char *line);
 
-/* Helper functions that are used to determine types of tokens */
+/**Functions that are used to determine types of tokens */
 int find_index(char *token, const char *arr[], int n);
 int find_command(char *token);
 int find_directive(char *token);
@@ -17,17 +17,17 @@ boolean is_string(char *string);
 boolean is_number(char *seq);
 boolean is_register(char *token);
 
-/* Helper functions that are used for creating files and assigning required extensions to them */
+/**Functions that are used for creating files with matching extentions */
 char *create_file_name(char *original, int type);
 FILE *open_file(char *filename, int type);
 char *convert_to_base_32(unsigned int num);
 
-/* Functions of external labels positions' linked list */
+/** Functions of external labels and linked list */
 extPtr add_ext(extPtr *hptr, char *name, unsigned int reference);
 void free_ext(extPtr *hptr);
 void print_ext(extPtr h);
 
-/* Functions of symbols table */
+/** Functions of symbols table */
 labelPtr add_label(labelPtr *hptr, char *name, unsigned int address, boolean external, ...);
 int delete_label(labelPtr *hptr, char *name);
 void free_labels(labelPtr *hptr);
@@ -39,11 +39,11 @@ boolean is_external_label(labelPtr h, char *name);
 int make_entry(labelPtr h, char *name);
 void print_labels(labelPtr h);
 
-/* Functions that handle errors */
+/** Functions that handle errors */
 void write_error(int line_num); /* This function is called when an error output is needed */
 int is_error();
 
-/* Helper functions for encoding and building words */
+/**Functions for building words */
 unsigned int extract_bits(unsigned int word, int start, int end);
 void encode_to_instructions(unsigned int word);
 unsigned int insert_are(unsigned int info, int are);
